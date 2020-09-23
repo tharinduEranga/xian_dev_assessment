2d5d7579f13e1b1a42b0d80b0783d4bd12854df2
tharindu-e <tharindue@ceyentra.com>
2020-09-23 23:44:55 +0530

    added calculate price API end point

--- src/main/java/com/xian/assessment/controller/CartonController.java
+++ src/main/java/com/xian/assessment/controller/CartonController.java
@@ -1,15 +1,15 @@
 package com.xian.assessment.controller;
 
+import com.xian.assessment.model.dto.CalculateRequest;
 import com.xian.assessment.model.dto.CartonDTO;
 import com.xian.assessment.model.dto.CommonResponse;
 import com.xian.assessment.service.CartonService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
@@ -23,5 +23,10 @@ public class CartonController {
     @GetMapping()
     public ResponseEntity<CommonResponse<List<CartonDTO>>> getCartons() {
         return ResponseEntity.ok(new CommonResponse<>(cartonService.getCartonPrices()));
+    }
+
+    @PostMapping(value = "/calculate")
+    public ResponseEntity<CommonResponse<BigDecimal>> getCartonCalculateAmount(@RequestBody CalculateRequest calculateRequest) {
+        return ResponseEntity.ok(new CommonResponse<>(cartonService.getCalculatedPrice(calculateRequest)));
     }
 }
